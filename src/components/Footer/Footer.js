6bc436d686ce848b3e7fd99aea72e34d101b70bb
Anitkumarsingh <rockymaster118@gmail.com>
2018-10-05 12:29:12 +0530

    clearn up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import './Footer.css';
+import myPicture from '../../Images/myPic.jpg';
 // import Card from '../Card/Card';
 
 const Footer = () =>{
     return(
         <div className="useLess">
         <div className="footer-box">
-            <p style={{textAlign: '-webkit-center', fontSize:'22px',color:'white'}}>
+        <img className="myPic" src={myPicture} width="20%" height="auto" alt="" />
+            <p className="footer-text"style={{textAlign: '-webkit-center', fontSize:'22px',color:'white'}}>
               &copy;2018 Build By Anit Kumar
             </p>
         </div>
-        {/* <div class="container">
-        <img src="http://www.myresumeweb.pe.hu/wp-content/uploads/2018/02/tes-copy.jpg" alt=""/>
-        <img src="http://www.myresumeweb.pe.hu/wp-content/uploads/2018/02/tes-copy.jpg" alt=""/>
-        <img src="http://www.myresumeweb.pe.hu/wp-content/uploads/2018/02/tes-copy.jpg" alt=""/>
-        <img src="http://www.myresumeweb.pe.hu/wp-content/uploads/2018/02/tes-copy.jpg" alt=""/>
-          
-        <h2>ANIT KUMAR</h2>
-      <p className="description">    
-      I'm 2017 postgraduate MCA student from RNS Institute Of Technology, have worked as a Web Developer over 1 year. I enjoy developing elegant and functional user interfaces and full stack applications using both front-end and back-end JavaScript based technologies.
-      I am passionate about learning new technologies and frameworks and utilizing them in my work.</p>
-      </div> */}
       </div>
     );
 }
