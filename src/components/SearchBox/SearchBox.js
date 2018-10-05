9d1b0575c480bbf33baa8ae20f229a0eae158303
Anitkumarsingh <rockymaster118@gmail.com>
2018-10-05 10:04:24 +0530

    added some text

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -20,14 +20,17 @@ class SearchBox extends Component{
   render(){
     return(
           <div className="search-container">
+          
             <div className="search-box">
+            <h1 style={{color:'#ee6488',fontWeight:'bolder'}}>Welcome,to <span style={{fontSize:'25px',color:'white'}}>photos search engine</span></h1>
             <form onSubmit={this.handleSubmit}>
-              <input type="search" placeholder="Type to search..." 
+              <input type="search" placeholder="Type to search and hit enter key..." 
                 onChange={this.onSearchChange}
                 ref={input => (this.query = input)}
                 name="search"
               />
               </form>
+              <p style={{color:'white',fontSize:'22px;'}}>Search anything: Computer, love, rose ...</p>
             </div>
           </div>
      )
